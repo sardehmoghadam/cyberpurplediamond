e236d3b00040fa3c6f939fddfd1d38b5ed4d09a8
root <binaloodir@gmail.com>
2021-01-20 01:17:59 -0500

    Html form

--- helloworld/hello/views.py
+++ helloworld/hello/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
+from django import forms
 from django.http import HttpResponse
 # Create your views here.
-
+flights = ["First", "Second"]
+class newflightform(forms.Form):
+    flight = forms.CharField(label="Flight Name")
 def index(request):
     return render(request, "hello/index.html", {
-        # "test": False
+        "flights": flights
+    })
+def add(request):
+    if request.method == "POST":
+        form = newflightform(request.POST)
+        if form.is_valid():
+            flight = form.cleaned_data["flight"]
+            flights.append(flight)
+        else:
+            return render(request, "hello/add.html",{
+                "form": form
+            })
+    return render(request, "hello/add.html", {
+        "form": newflightform()
     })
 
 # def index(request):
